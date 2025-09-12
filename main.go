d9f1776866d518101e259156b4781e05bd930151
GauravKesh <gkrcoder@gmail.com>
2025-09-12 08:39:22 +0530

    arithmetic and control flow

--- main.go
+++ main.go
@@ -1,12 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func try() {
-	var IntNum int64 = 546
-	fmt.Println(IntNum)
+func learnArithmeticOperator() {
+	//  addition
+	const a int64 = 56
+	const b int64 = 54
+
+	const add int64 = a + b
+	var minus int64 = a - b
+	var mult int64 = a * b
+
+	fmt.Println(add)
+	fmt.Println(minus)
+	fmt.Println(mult)
+}
+
+func learnControlFlow() {
+	const num int = 3
+
+	variable := "string"
+
+	for i := 0; i < num; i++ {
+		fmt.Print(i)
+		fmt.Printf("the user name is %v", variable)
+	}
+
 }
 
 func main() {
-	try()
+	learnArithmeticOperator()
+	learnControlFlow()
 }
